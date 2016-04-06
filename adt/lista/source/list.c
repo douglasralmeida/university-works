3e8a10e7a1e07823b02c791370932d774207d6a4
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-04-06 11:17:23 -0300

    First code in list.c

--- adt/lista/source/list.c
+++ adt/lista/source/list.c
@@ -2,6 +2,41 @@
 //LIST ABSTRACT DATA TYPE
 //DOUGLAS RODRIGUES DE ALMEIDA
 //
-//List ADT Implementation
+//LinkedList ADT Implementation
 
 #include "list.h"
+
+TList* TList_Create(void)
+{
+  TList* TempList = (TList*)malloc(sizeof(TList));
+  TempList->First = NULL;
+  TempList->Last = NULL;
+  
+  return TempList;
+}
+
+void TList_Destroy(TList** PList)
+{
+  free(*PList);
+  PList = NULL;
+}
+
+void TList_Add(TList* List, int Value)
+{
+  
+}
+
+void TList_Clear(TList* List)
+{
+  
+}
+
+void TList_Insert(TList* List, int Value, int Position)
+{
+  
+}
+
+void TList_Remove(TList* List, int Position)
+{
+  
+}
