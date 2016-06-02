4e139717e943ede8e9f84c0f405a281fa24f13b4
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-06-02 00:37:54 -0300

    TP1 de AEDS2. Tipo booleano

---
+++ tps/serv-impressao/include/boolutils.h
@@ -0,0 +1,17 @@
+/*
+**	TIPO BOOLEANO
+**	DOUGLAS RODRIGUES DE ALMEIDA
+**
+**	Tipo booleano para o padrao C89
+**	
+**/
+
+#ifndef BOOLUTILS_H
+#define BOOLUTILS_H
+
+/* Emulacao do tipo booleano */
+typedef enum {
+	false, true
+	} bool;
+
+#endif
