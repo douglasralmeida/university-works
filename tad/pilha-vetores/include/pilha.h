4708d3c27ab681bd94a103a1af286aa3798e779b
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-08-04 09:16:07 -0300

    TAD Pilha por Vetor. Implementação de uma pilha usando vetores.

---
+++ tad/pilha-vetores/include/pilha.h
@@ -0,0 +1,87 @@
+/*
+ *	TIPO ABSTRATO DE DADOS PILHA
+ *	DOUGLAS RODRIGUES DE ALMEIDA
+ *
+ *	Cabeçalhos e estruturas de uma pilha por vetores
+ */
+
+#ifndef pilha_H
+#define pilha_H
+
+#include "boolutils.h"
+#include "core.h"
+
+#define PILHA_EXPANSAO 1024
+
+/* Nó da pilha */
+typedef size_t TPilhaNo;
+
+/* Estrutura da pilha */
+typedef struct _TPilha {
+	size_t Capacidade;	/*-- Capacidade de armazenamento atual da pilha --*/
+	size_t Expansao;	/*-- Capacidade de autoexpansão da pilha --*/
+	TPilhaNo Frente;	/*-- Primeiro item da pilha --*/
+	void** Itens;		/*-- Conteúdo da pilha --*/
+} TPilha;
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Criar
+ * 			Cria uma pilha vazia
+ * @param:		Capacidade inicial da pilha
+ * @retorna:		A pilha alocada
+ *---------------------------------------------------------------------------*/ 
+TPilha* TPilha_Criar(size_t Capacidade);
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Destruir
+ * 			Destroi a pilha
+ * @param:		Pilha
+ * @param:		Funcao que destroi um item da pilha
+ * @retorna:		(vazio)
+ *---------------------------------------------------------------------------*/ 
+void TPilha_Destruir(TPilha** PPilha, TFuncaoDestruir FuncaoDestruir);
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Desempilhar
+ * 			Remove e retorna o proximo item da pilha
+ * @param:		Pilha
+ * @retorna:		Item removido
+ *---------------------------------------------------------------------------*/ 
+void* TPilha_Desempilhar(TPilha* Pilha);
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Empilhar
+ * 			Insere um item no final da pilha
+ * @param:		Pilha
+ * @param:		Item a ser inserido
+ * @retorna:		(vazio)
+ *---------------------------------------------------------------------------*/ 
+bool TPilha_Empilhar(TPilha* Pilha, void* Item);
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Imprimir
+ * 			Imprime a pilha na tela em ordem de saída
+ * @param:		Pilha
+ * @param:		Funcao que imprime um item da pilha
+ * @retorna:		(vazio)
+ *---------------------------------------------------------------------------*/ 
+void TPilha_Imprimir(TPilha* Pilha, TFuncaoImprimir FuncaoImprimir);
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Limpar
+ * 			Remove todos os itens da pilha
+ * @param:		Pilha
+ * @param:		Funcao que destroi um item da pilha
+ * @retorna:		(vazio)
+ *---------------------------------------------------------------------------*/ 
+void TPilha_Limpar(TPilha* Pilha, TFuncaoDestruir FuncaoDestruir);
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TPilha_Tamanho
+ * 			Retorna o quantidade de itens da pilha
+ * @param:		Pilha
+ * @retorna:		Inteiro com o tamanho
+ *---------------------------------------------------------------------------*/ 
+size_t TPilha_Tamanho(TPilha* Pilha);
+
+#endif
---
+++ tad/pilha-vetores/src/pilha.c
@@ -0,0 +1,107 @@
+/*
+**	TIPO ABSTRATO DE DADOS PILHA
+**	DOUGLAS RODRIGUES DE ALMEIDA
+**
+**	Implementação de pilha com vetor
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+TPilha* TPilha_Criar(size_t Capacidade)
+{
+	TPilha* NovaPilha;
+	
+	NovaPilha = (TPilha*)malloc(sizeof(TPilha));
+	if (NovaPilha == NULL)
+	{
+		printf("Erro (0x31): Erro durante alocacao de memoria.\n");
+		return NULL;
+	}
+	NovaPilha->Itens = malloc((Capacidade) * sizeof(void*));
+	if (NovaPilha->Itens == NULL)
+	{
+		printf("Erro(0x34): Erro durante alocacao de memoria.\n");
+		free(NovaPilha);
+		return NULL;
+	}
+	NovaPilha->Capacidade = Capacidade;
+	NovaPilha->Expansao = PILHA_EXPANSAO;
+	NovaPilha->Frente = 0;
+  
+	return NovaPilha;
+}
+
+void TPilha_Destruir(TPilha** PPilha, TFuncaoDestruir FuncaoDestruir)
+{
+	if (PPilha != NULL)
+	{
+		TPilha_Limpar(*PPilha, FuncaoDestruir);
+		free((*PPilha)->Itens);
+		free(*PPilha);
+		*PPilha = NULL;
+	}
+}
+
+void* TPilha_Desempilhar(TPilha* Pilha)
+{
+	void* Item;
+	
+	if (Pilha->Frente == 0)
+	{
+		printf("Erro (0x26): Erro ao desempilhar. Pilha vazia.\n");
+		return NULL;
+	}
+	else
+	{
+		Item = Pilha->Itens[Pilha->Frente - 1];
+		Pilha->Frente--;
+		return Item;
+	}
+}
+
+bool TPilha_Empilhar(TPilha* Pilha, void* Item)
+{
+	if (Pilha->Frente == Pilha->Capacidade)
+	{
+		printf("Erro: (0x25): Erro ao empilhar. Pilha cheia.\n");
+		return false;
+	}
+	else 
+	{
+		Pilha->Itens[Pilha->Frente] = Item;
+		Pilha->Frente++;
+		return true;
+	}
+}
+
+void TPilha_Imprimir(TPilha* Pilha, TFuncaoImprimir FuncaoImprimir)
+{
+	TPilhaNo NoTemp;
+	
+	NoTemp = Pilha->Frente;
+	while (NoTemp > 0)
+	{
+		FuncaoImprimir(Pilha->Itens[NoTemp - 1]);
+		NoTemp--;
+	}
+}
+
+void TPilha_Limpar(TPilha* Pilha, TFuncaoDestruir FuncaoDestruir)
+{
+	TPilhaNo NoTemp;
+	
+	NoTemp = Pilha->Frente;
+	while (NoTemp > 0)
+	{
+		FuncaoDestruir(&Pilha->Itens[NoTemp - 1]);
+		NoTemp--;
+	}
+	Pilha->Frente = 0;
+}
+
+size_t TPilha_Tamanho(TPilha* Pilha)
+{
+	return Pilha->Frente;
+}
