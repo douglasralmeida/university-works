0b0261584bcc32c73f25d6e7c5d6cfed8f661bd5
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-08-05 01:02:42 -0300

    TAD Matriz. Implementação inicial de uma matriz com vetores.

---
+++ tad/matriz-vetores/src/matriz.c
@@ -0,0 +1,33 @@
+/*
+ *	TIPO ABSTRATO DE DADOS MATRIZ
+ *	DOUGLAS RODRIGUES DE ALMEIDA
+ *
+ *	Implementação de uma matriz com vetores
+ */
+
+#include "matriz.h"
+
+TMatriz* TMatriz_Criar(size_t NumLinhas, size_t NumColunas)
+{
+
+}
+
+void TMatriz_Destruir(TMatriz** PMatriz)
+{
+
+}
+
+TMatriz* TMatriz_Adicionar(TMatriz* MatrizA, TMatriz* MatrizB)
+{
+
+}
+
+TMatriz* TMatriz_Multiplicar(TMatriz* MatrizA, TMatriz* MatrizB)
+{
+
+}
+
+TMatriz* TMatriz_Subtrair(TMatriz* MatrizA, TMatriz* MatrizB)
+{
+
+}
