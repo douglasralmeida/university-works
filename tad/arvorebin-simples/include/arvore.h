a467caa3154dff499bb1aa243bad41a00acd5e78
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-08-18 18:43:07 -0300

    TAD Arvore Binaria Simples. Implementação parcial.

---
+++ tad/arvorebin-simples/include/arvore.h
@@ -0,0 +1,39 @@
+/*
+ *	TIPO ABSTRATO DE DADOS ARVORE BINARIA
+ *	DOUGLAS RODRIGUES DE ALMEIDA
+ *
+ *	Cabeçalhos e estruturas de uma arvore binaria sem balaceamento
+ */
+
+#ifndef ARVORE_H
+#define ARVORE_H
+
+/* Estrutura do nó da arvore */
+typedef struct _TArvoreNo* TArvoreNo;
+
+struct _TArvoreNo {
+	void* Item;			/* Guarda o item da fila */
+	TArvoreNo Esquerda;	/* Aponta para o filho esquerdo do nó */
+	TArvoreNo Direita;	/* Aponta para o filho direito do nó */
+};
+
+typedef TArvoreNo TArvore;
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TArvore_Criar
+ * 				Cria uma arvore vazia
+ * @param:		(vazio)
+ * @retorna:	A arvore alocada
+ *---------------------------------------------------------------------------*/ 
+TArvore TArvore_Criar();
+
+/* ----------------------------------------------------------------------------
+ * funcao:		TArvore_Destruir
+ * 				Destroi a arvore
+ * @param:		Arvore
+ * @param:		Funcao que destroi um item do nó da árvore
+ * @retorna:	(vazio)
+ *---------------------------------------------------------------------------*/ 
+void TArvore_Destruir(TArvore* PArvore, TFuncaoDestruir FuncaoDestruir);
+
+#endif
