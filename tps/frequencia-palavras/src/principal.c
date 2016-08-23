972c99a7a7f1cebdcfe79b9846d6401993695180
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-08-22 22:39:10 -0300

    TP0 de AEDS3. Início do trabalho.

---
+++ tps/frequencia-palavras/src/principal.c
@@ -0,0 +1,14 @@
+/*
+**	FREQUENCIA DE PALAVRAS
+**	DOUGLAS RODRIGUES DE ALMEIDA
+**
+**	Aplicativo principal
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	exit(EXIT_SUCCESS);
+}
