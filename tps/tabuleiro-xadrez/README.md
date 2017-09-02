15210f4906f0e58e6681ed61a2ce6908299ff093
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2017-09-02 16:15:16 -0300

    TP1 de PM. Início dos trabalhos.

---
+++ tps/tabuleiro-xadrez/README.md
@@ -0,0 +1,14 @@
+# TP1 de PM
+Nome: Tabuleiro de Xadrez
+
+Assunto: Programação Orientada a Objetos
+
+Linguagem: Java
+
+Bibliotecas necessárias: Nenhuma
+
+Compilador: Java 8
+
+Ambiente: Windows
+
+Disciplina: PM
---
+++ tps/tabuleiro-xadrez/src/Xadrez.java
@@ -0,0 +1,16 @@
+/**
+ *	TP1 de PM
+ *	Tabuleiro de Xadrez
+ *	@author Douglas Rodrigues 
+ */
+
+public class Xadrez {
+	/**
+	 * @param args: Argumentos do aplicativo
+	 */
+	public static void main(String[] args) {
+		System.out.println("<matriz_representativa>");
+		System.out.println("<casas_visitadas>");
+		System.out.println("<tempo_gasto>");
+	}
+}
