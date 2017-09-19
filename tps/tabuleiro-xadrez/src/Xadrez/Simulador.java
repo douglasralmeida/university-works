f5d2793cd4d1be3b5fdee82a6113c5db853fc2d4
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2017-09-19 17:22:49 -0300

    TP1 de PM. Exigência do TP para implementar tudo em um arquivo único

--- tps/tabuleiro-xadrez/src/Xadrez/Simulador.java
+++ tps/tabuleiro-xadrez/src/Xadrez/Simulador.java
@@ -13,7 +13,179 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class Simulador {
+class Simulador {
+	interface Peca {
+		Point getMovimento(int i);
+		Point getPosicao();
+		Point getProxPosicao();
+		int getQtMovimentos();
+		void setPosicao(Point posicao);
+		void setProxPosicao(int movimento);
+	}
+	
+	class Cavalo implements Peca {
+		public final Point[] movimentos;
+		private Point posicao;
+		private Point proxposicao;
+		
+		/**
+		 * Construtor do Cavalo
+		 */
+		public Cavalo() {
+			movimentos = new Point[] {
+					new Point(1, 2),
+					new Point(2, 1),
+					new Point(2, -1),
+					new Point(1, -2),
+					new Point(-1, -2),
+					new Point(-2, -1),
+					new Point(-2, 1),
+					new Point(-1, 2)
+			};
+			this.posicao = new Point(-1, -1);
+			this.proxposicao = new Point(-1, -1);
+		}
+		
+		/**
+		 * Obtem um movimento possivel 
+		 */
+		@Override
+		public Point getMovimento(int i) {
+			return movimentos[i];
+		}
+
+		/**
+		 * Obtem a quantidade de movimentos possiveis 
+		 */
+		@Override
+		public int getQtMovimentos() {
+			return movimentos.length;
+		}
+
+		/**
+		 * Obtem a posicao da peça no tabuleiro 
+		 */
+		@Override
+		public Point getPosicao() {
+			return posicao;
+		}
+		
+		/**
+		 * Obtem a próxima posição da peça para ser movimentada
+		 */
+		@Override
+		public Point getProxPosicao() {
+			return proxposicao;
+		}
+
+		/**
+		 * Altera a posição da peça 
+		 */
+		@Override
+		public void setPosicao(Point posicao) {
+			this.posicao = posicao;
+		}
+		
+		/**
+		 * Altera a próxima posição da peça
+		 * @param movimento: id do movimento possível 
+		 */
+		@Override
+		public void setProxPosicao(int movimento) {
+			proxposicao.setLocation(posicao.x + movimentos[movimento].x, posicao.y + movimentos[movimento].y);
+		}
+	}
+	
+	class Tabuleiro {
+		private int altura;
+		private int largura;
+		private int numvisitas;
+		private int[][] casas;
+		
+		/**
+		 * Construtor do Tabuleiro
+		 * @param largurainicial: Largura inicial do tabuleiro
+		 * @param alturainicial: Altura inicial do tabuleiro
+		 */
+		public Tabuleiro(int largurainicial, int alturainicial) {
+			numvisitas = 0;			
+			casas = new int[largurainicial][alturainicial];
+			altura = alturainicial;
+			largura = largurainicial;
+		}
+		
+		/**
+		 * Obtem a altura do tabuleiro
+		 */
+		public int getAltura() {
+			return altura;
+		}
+		
+		/**
+		 * Obtem a largura do tabuleiro
+		 */
+		public int getLargura() {
+			return largura;
+		}
+		
+		/**
+		 * Imprime o tabuleiro na tela
+		 */
+		void imprimir() {
+			int i, j;
+			
+			for (i = getAltura() - 1; i >= 0; i--) {
+				for (j = 0; j < getLargura(); j++) {
+					if (casas[i][j] < 10)
+						System.out.print(' ');
+					System.out.print(casas[i][j]);
+					System.out.print(' ');
+				}
+				System.out.print('\n');
+			}
+		}
+		
+		/**
+		 * Imprime detalhes do tabuleiro na tela
+		 */
+		void imprmirDetalhes() {
+			System.out.println(numvisitas);
+		}
+		
+		/**
+		 *  Insere uma peça no tabuleiro na posição indicada
+		 *  @param peca: peca que será inserida
+		 *  @param posicao: posicao onde será inserida
+		 */
+		void inserir(Peca peca, Point posicao) {
+			casas[posicao.x][posicao.y] = numvisitas+1;
+			peca.setPosicao(posicao);
+			numvisitas++;
+		}
+		
+		/**
+		 * Movimenta uma peca no tabuleiro para a posicao indicada
+		 * @param peca: peca que está sendo movimentada
+		 */
+		void movimentar(Peca peca) {
+			casas[peca.getProxPosicao().x][peca.getProxPosicao().y] = numvisitas+1;
+			peca.setPosicao(peca.getProxPosicao());
+			numvisitas++;
+		}
+		
+		/**
+		 * Checa se um casa do tabuleiro pode ser visitada
+		 * @param p: posicao da casa
+		 */		
+		boolean podeMovimentar(Point p) {				
+			return ((p.x < getLargura()) &&
+					(p.y < getAltura()) &&
+					(p.x >= 0) &&
+					(p.y >= 0) &&
+					(casas[p.x][p.y] == 0));
+		}
+	}
+	
 	private Tabuleiro tab;
 	private Instant tempoInicial;
 	private Duration tempoTotal;
