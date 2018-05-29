f30aed899cc4beb071fb7726847f604f777aedd9
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-05-29 01:23:23 -0300

    TP3 de SO. Reorganização do código.

--- tps/fsshell/src/fs.c
+++ tps/fsshell/src/fs.c
@@ -12,22 +12,27 @@
 #include "fcntl.h"
 #include "fs.h"
 
+struct {
+  char* imgnome;
+  int   imgdesc;
+} fs;
+
 int img_open(char* nomearq) {
-  int fd = -1;
-  
-  fd = open(nomearq, O_RDONLY);
-  if (fd == -1) {
+  fs.imgdesc = -1;
+  fs.imgdesc = open(nomearq, O_RDONLY);
+  if (fs.imgdesc == -1) {
       perror("Falha ao abrir imagem.");
       return 0;
-   } else {
-     return 1;
    }
+  fs.imgnome = nomearq;
+    
+  return 1;
 }
 
-int sh_getcmd(char* nomeimg, char *buf, int nbuf) {
+int sh_getcmd(char *buf, int nbuf) {
   /* Checa se o stdin é um terminal */
   if (isatty(fileno(stdin)))
-    fprintf(stdout, "%s> ", nomeimg);
+    fprintf(stdout, "%s> ", fs.imgnome);
   memset(buf, 0, nbuf);
   fgets(buf, nbuf, stdin);
   if (buf[0] == 0) /* EOF (hora de ir embora, tchau) */
@@ -35,20 +40,22 @@ int sh_getcmd(char* nomeimg, char *buf, int nbuf) {
   return 1;
 }
 
-int main(int argc, char *argv[]) {
+void sh_run() {
   static char buf[SHBUFFER];
-  char* arquivoimagem;
- 
-  if (argc < 2) {
-    fprintf(stdout, "Número de argumentos insuficientes.\n");
-    exit(EXIT_FAILURE);
-  }
-  arquivoimagem = argv[1];
-  if (!img_open(arquivoimagem)) {
-    exit(EXIT_FAILURE);
-  }
-  while (sh_getcmd(arquivoimagem, buf, sizeof(buf))) {
+
+  while (sh_getcmd(buf, sizeof(buf))) {
     
   }
+}
+
+int main(int argc, char *argv[]) { 
+  if (argc < 2) {
+    fprintf(stdout, "A sintaxe do comando está incorreta.\n");
+    exit(EXIT_FAILURE);
+  }
+  if (!img_open(arqv[1])) {
+    exit(EXIT_FAILURE);
+  }
+  sh_run();
   exit(EXIT_SUCCESS);
 }
