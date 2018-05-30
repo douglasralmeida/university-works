616811cbd2c18f9cac6800de3ab084ff08b3ff00
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-05-30 01:14:27 -0300

    TP3 de SO. Estruturas para os comandos do shell.

--- tps/fsshell/src/fs.c
+++ tps/fsshell/src/fs.c
@@ -5,6 +5,7 @@
 **
 */
 
+#include "ctype.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -12,13 +13,28 @@
 #include "fcntl.h"
 #include "fs.h"
 
-struct {
-  char* imgnome;
-  int   imgdesc;
-} fs;
+void str_trim(char *str) {
+  int i;
+  int inicio = 0;
+  int final = strlen(str) - 1;
+  while (isspace((unsigned char) str[inicio]))
+    inicio++;
+  while ((final >= inicio) && isspace((unsigned char) str[final]))
+    final--;
+
+  /* Empurra todos os caracterse para o inicio da cadeia. */
+  for (i = inicio; i <= final; i++)
+    str[i - inicio] = str[i];
+  str[i - inicio] = '\0';
+}
+
+void fs_init() {
+  fs.imgdesc = -1;
+  fs.pwd[0] = '/';
+  fs.pwd[1] = '\0';
+}
 
 int img_open(char* nomearq) {
-  fs.imgdesc = -1;
   fs.imgdesc = open(nomearq, O_RDONLY);
   if (fs.imgdesc == -1) {
       perror("Falha ao abrir imagem.");
@@ -27,6 +43,31 @@ int img_open(char* nomearq) {
   fs.imgnome = nomearq;
     
   return 1;
+}
+
+void sh_init() {
+  shcmds.functions = shcmds_func;
+  shcmds.names = shcmds_name;
+}
+
+void sh_cmd_pwd() {
+  fputs(fs.pwd, stdout);
+}
+
+int shcmds_lookup(char* cmd) {
+  int i;
+  
+  for (i = SHCMDSMAX - 1; i >= 0; i--) {
+    if (!strcmp(cmd, shcmds.names[i]))
+      return i;
+  }
+  
+  return i;
+}
+
+void shcmds_call(int index) {
+  shcmds.functions[index]();
+  fputc('\n', stdout);
 }
 
 int sh_getcmd(char *buf, int nbuf) {
@@ -42,9 +83,17 @@ int sh_getcmd(char *buf, int nbuf) {
 
 void sh_run() {
   static char buf[SHBUFFER];
+  int cmdindex;
 
   while (sh_getcmd(buf, sizeof(buf))) {
-    
+    str_trim(buf);
+    /* comando exit */
+    if (buf[0] == 'e' && buf[1] == 'x' && buf[2] == 'i' && buf[3] == 't' && buf[4] == '\0')
+      return;
+      
+    cmdindex = shcmds_lookup(buf);
+    if (cmdindex >= 0)
+      shcmds_call(cmdindex);
   }
 }
 
@@ -53,7 +102,9 @@ int main(int argc, char *argv[]) {
     fprintf(stdout, "A sintaxe do comando está incorreta.\n");
     exit(EXIT_FAILURE);
   }
-  if (!img_open(arqv[1])) {
+  fs_init();
+  sh_init();
+  if (!img_open(argv[1])) {
     exit(EXIT_FAILURE);
   }
   sh_run();
--- tps/fsshell/src/fs.h
+++ tps/fsshell/src/fs.h
@@ -8,7 +8,51 @@
 #ifndef FS_H
 #define FS_H
 
-/* Buffer da linha de comando atual */
+/* Tamanho do buffer da linha de comando atual */
 #define SHBUFFER 100
+
+typedef unsigned short uint16;
+
+typedef unsigned int uint32;
+
+struct ext2_super_block {
+  uint32 inodes_count;          /* Contador de Inodes */
+};
+
+struct {
+  char* imgnome;
+  int   imgdesc;
+  char  pwd[129];
+} fs;
+
+/****  COMANDOS DO SHELL  ****/
+
+/* Quantidade de comandos */
+#define SHCMDSMAX 1
+
+/* Cabeçalho dos comandos */
+void sh_cmd_pwd(void);
+
+/* Estruturas para o vetor de comandos do shell */
+typedef void (*shcmd)(void);
+
+shcmd shcmds_func[SHCMDSMAX] = {
+  &sh_cmd_pwd
+};
+
+/* Cada string deve estar na mesma posição da
+   sua respectiva função em shcmds_func */
+char* shcmds_name[SHCMDSMAX] = {
+  "pwd"
+};
+
+static struct {
+  shcmd* functions;
+  char** names;
+} shcmds;
+
+int shcmds_lookup(char* cmd);
+
+void shcmds_call(int index);
 
 #endif
